e063a58d26cee67f8811ce2366a90ab4a6c74b95
Giorge Abdala <giorgeabdala@gmail.com>
2023-04-27 11:11:36 -0300

    refactor: corrigido fronted para add e atualizar clientes. compose corrigido para persistir os dados corretos no container do whats.

--- frontend/src/components/ClientListChacraUi/clientListChacra.tsx
+++ frontend/src/components/ClientListChacraUi/clientListChacra.tsx
@@ -10,6 +10,16 @@ import {
     Input,
     Button,
     useToast,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    FormControl,
+    FormLabel,
+    useDisclosure,
 } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -25,10 +35,13 @@ interface Client {
 function ClientListChacra() {
     const [clients, setClients] = useState<Client[]>([]);
     const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [DDD, setDDD] = useState("");
-    const [number, setNumber] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
 
     const toast = useToast();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     useEffect(() => {
         async function fetchClients() {
@@ -69,6 +82,30 @@ function ClientListChacra() {
         }
     }
 
+    async function handleAddClient() {
+        try {
+            const newClient = { firstName, lastName, DDD, phone, email };
+            const response = await axios.post("http://localhost:3000/client", newClient);
+            setClients([...clients, response.data.body]);
+            toast({
+                title: "Cliente adicionado com sucesso!",
+                status: "success",
+                duration: 3000,
+                isClosable: true,
+            });
+            onClose();
+        } catch (error) {
+            console.log(error);
+            toast({
+                title: "Erro ao adicionar cliente",
+                description: "Verifique os dados e tente novamente",
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+    }
+
     function handleFirstNameChange(event: React.ChangeEvent<HTMLInputElement>, client: Client) {
         const updatedClients = clients.map((c) => {
             if (c.id === client.id) {
@@ -76,6 +113,7 @@ function ClientListChacra() {
             }
             return c;
         });
+
         setClients(updatedClients);
     }
 
@@ -86,6 +124,7 @@ function ClientListChacra() {
             }
             return c;
         });
+
         setClients(updatedClients);
     }
 
@@ -104,30 +143,79 @@ function ClientListChacra() {
 
 
 
+
     return (
         <ChakraProvider>
-            <Box textAlign="center" fontSize="xl">
-                <Heading>Listagem de Clientes</Heading>
-                <Table mt="4" size="md" variant="simple">
+            <Box>
+                <Heading>Listagem de clientes</Heading>
+                <Button colorScheme="blue" onClick={onOpen}>
+                    Adicionar
+                </Button>
+                <Table>
                     <Tbody>
-                        {clients.map((client : Client) => (
+                        {clients.map((client) => (
                             <Tr key={client.id}>
                                 <Td>{client.id}</Td>
-                                <Td> <Input value={client.firstName} onChange={(event) => handleFirstNameChange(event, client)}
-                                /> </Td>
-                                <Td>
-                                    <Input value={client.DDD} onChange={(event) => handleDDDChange(event, client)}/></Td>
-                                <Td>
-                                    <Input value={client.phone} onChange={(event) => handleNumberChange(event, client)}/> </Td>
+                                <Td> <Input value={client.firstName} onChange={(event) => handleFirstNameChange(event, client)}/> </Td>
+                                <Td>{client.lastName}</Td>
+                               <Td><Input value={client.DDD} onChange={(event) => handleDDDChange(event, client)}/></Td>
+                                <Td> <Input value={client.phone} onChange={(event) => handleNumberChange(event, client)}/> </Td>
+                                <Td>{client.email}</Td>
                                 <Td>
                                     <Button colorScheme="pink" onClick={() => handleUpdateClient(client)}>
-                                        Atualizar
+                                        Alterar
                                     </Button>
                                 </Td>
                             </Tr>
                         ))}
                     </Tbody>
                 </Table>
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Adicionar Cliente</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <FormControl>
+                                <FormLabel>Nome</FormLabel>
+                                <Input
+                                    value={firstName}
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Sobrenome</FormLabel>
+                                <Input
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>DDD</FormLabel>
+                                <Input value={DDD} onChange={(e) => setDDD(e.target.value)} />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>TelefoneS</FormLabel>
+                                <Input
+                                    value={phone}
+                                    onChange={(e) => setPhone(e.target.value)}
+                                />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Email</FormLabel>
+                                <Input
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </FormControl>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme="blue" onClick={handleAddClient}>
+                                Adicionar
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
             </Box>
         </ChakraProvider>
     );
