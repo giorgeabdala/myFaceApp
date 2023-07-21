9a2dbff657f2f18b9ce2972a0971db345eb47299
Giorge Abdala <giorgeabdala@gmail.com>
2023-07-20 21:18:48 -0300

    migração do fronted para o Next.Js

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './nest/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
 
